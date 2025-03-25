8fbbac4149a8b95bad4c0a9f533de88e39c075d5
EmreKuzey04 <emrekuzey88@gmail.com>
2025-03-25 03:15:34 +0300

    Delete Areas/Admin directory

--- Areas/Admin/Models/Dtos/ShipperAddDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WEB.MVCUI.Areas.Admin.Models.Dtos
-{
-    public class ShipperAddDto
-    {
-        public string ShipperName { get; set; }
-        public string Phone { get; set; }
-    }
-}
--- Areas/Admin/Models/Dtos/UserLogInDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WEB.MVCUI.Areas.Admin.Models.Dtos
-{
-    public class UserLogInDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
