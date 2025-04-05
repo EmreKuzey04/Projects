73727d1b1d7df071c72bcda250f15304703a4a54
EmreKuzey04 <emrekuzey88@gmail.com>
2025-04-06 02:26:00 +0300

    s

--- E-Commerce/WEB.MVCUI/Models/Dtos/LogInAppUserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WEB.MVCUI.Models.Dtos
-{
-    public class LogInAppUserDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
--- E-Commerce/WEB.MVCUI/Models/Entities/Shipper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WEB.MVCUI.Models.Entities
-{
-    public class Shipper
-    {
-        public int ShipperID { get; set; }
-        public string ShipperName { get; set; }
-        public string Phone { get; set; }
-    }
-}
